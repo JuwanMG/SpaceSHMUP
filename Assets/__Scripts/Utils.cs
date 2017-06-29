9a9bbf1b202c997f2e34b3c74fca471830584c82
JuwanMG <JuwanMG@users.noreply.github.com>
2017-06-29 15:22:24 -0400

    Space SHMUP Update
    Completed Script for the firing and Damage frames for the Enemies

--- Assets/__Scripts/Utils.cs
+++ Assets/__Scripts/Utils.cs
@@ -215,6 +215,20 @@ public class Utils : MonoBehaviour
         return (FindTaggedParent(t.gameObject));
     }
 
+    static public Material[] GetAllMaterials(GameObject go)
+    {
+        List<Material> mats = new List<Material>();
+        if (go.GetComponent<Renderer>())
+        {
+            mats.Add(go.GetComponent<Renderer>().material);
+        }
+        foreach(Transform t in go.transform)
+        {
+            mats.AddRange(GetAllMaterials(t.gameObject));
+        }
+        return (mats.ToArray());
+    }
+
 }// End of Util Class
 
 
